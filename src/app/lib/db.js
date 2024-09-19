e64b9c0c93c6fb4b51d31541892162a89f35b2c4
theripper1116 <52823649+theripper1116@users.noreply.github.com>
2024-09-19 18:15:23 +0530

    Invoked environment files to hide username and password for MongoDB

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,2 +1,3 @@
-export const connectionStr =
-  "mongodb+srv://milanagarwallaj:Powerzone2024@cluster0.5wjgq.mongodb.net/restaurantDB?retryWrites=true&w=majority&appName=Cluster0";
+const { username, password } = process.env;
+
+export const connectionStr = `mongodb+srv://${username}:${password}@cluster0.5wjgq.mongodb.net/restaurantDB?retryWrites=true&w=majority&appName=Cluster0`;
